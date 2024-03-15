707e2edd34828a3c14050f0f13594309e8c5c5fb
Brian-kipkoech-mutai <kbrianmutai@gmail.com>
2024-03-15 05:14:50 +0300

    all in one  commit

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# calorie-counter
+
+# Calorie Counter App
+
+This is a simple web application for tracking calorie intake and expenditure. Users can input their budgeted calories and add entries for breakfast, lunch, dinner, snacks, and exercise. The app calculates the consumed and burned calories and displays the deficit or surplus relative to the budgeted calories.
+
+## Features
+
+- Allow users to input their budgeted calories.
+- Enable users to add entries for different categories such as breakfast, lunch, dinner, snacks, and exercise.
+- Calculate consumed calories based on the user's entries.
+- Calculate burned calories from exercise entries.
+- Display the deficit or surplus of calories relative to the budgeted calories.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+
+## How to Use
+
+1. Clone the repository:
+
+2. Open the `index.html` file in your web browser.
+
+3. Input your budgeted calories and add entries for your meals and exercise.
+
+4. Click on the "Get calories" button to calculate your consumed and burned calories.
+
+5. View the results to see your total calories, consumed calories, burned calories, and deficit or surplus.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+
+
---
+++ script.js
@@ -0,0 +1,121 @@
+ 
+const caloriesBudgetInput=document.querySelector('#caloriesBudgetInput');
+const entryOptions=document.querySelector('#entryOptions');
+const addEntryButton=document.querySelector('#addEntryButton');
+const form=document.querySelector('#form');
+const output=document.querySelector('.output');
+const caloriesBudgetText=document.querySelector('#caloriesBudgetText');
+const consumedCaloriesText=document.querySelector('#consumedCaloriesText');
+const burnedcaloriesText=document.querySelector('#burnedcaloriesText');
+const DeficitOrSurplusText=document.querySelector('#DeficitOrSurplusText');
+const submitButton=document.querySelector('#submitButton');
+const caloriesInput=document.querySelectorAll('#caloriesInput .inputSpan')
+const resetButton=document.querySelector('#resertButton')
+let isError =false;
+ 
+
+const addEntry=()=>{
+     const fieldId= `#${entryOptions.value}`;
+     const targetField=document.querySelector(fieldId);
+     const inputCount = document.querySelectorAll( `${fieldId} input[type='text']`).length+1
+     const  inputTemplate=`
+     <article>
+     <div class="inputPair">
+     <label for="${entryOptions.value}-${inputCount}-Name" class="labelClass">${entryOptions.value} ${inputCount} Name</label>
+     <input type="text" id="${entryOptions.value}-${inputCount}-Name" class="inputClass">
+ </div>
+ <div class="inputPair">
+     <label for="${entryOptions.value}-${inputCount}-Value" class="labelClass">${entryOptions.value} ${inputCount} Value</label>
+     <input type="number" min="0" id="${entryOptions.value}-${inputCount}-Value" class="inputClass">
+ </div>
+ 
+ </article>
+     `
+     
+    targetField.insertAdjacentHTML("beforeend",inputTemplate)
+       
+}
+ 
+ addEntryButton.onclick=addEntry;
+ const cleanInput=(input)=>{
+    console.log('input',input)
+    console.log('inputType',typeof(input));
+     const regex=/[+-\s]/
+     return input.replace(regex,'')
+     
+ }
+ const isValidInput=(input)=>{
+    const regex=/\d+e\d+/i;
+    return input.match(regex)
+   
+ }
+ console.log('isinvalid',isValidInput('1e2'));
+ 
+  const addCalories=(nodeList)=>{
+    const nodeArray=Array.from(nodeList)
+    console.log('nodeArray',nodeList.keys());
+    let caloriesTotal=0;
+     for (const element of nodeArray){
+         
+        console.log('element.values',element.value);
+        const cleaned=cleanInput(element.value);
+        const isInvalid= isValidInput(cleaned)
+        if(isInvalid){
+            isError=true;
+            alert(`Invalid Input ${element.value}`)
+            return;
+        }
+        else{
+       caloriesTotal+=Number(cleaned);
+        }
+
+        
+
+
+     }
+     return caloriesTotal;
+  }
+  const calculateCalories=()=>{
+  const breakfast=document.querySelectorAll('#breakfast input[type="number"]');
+  const dinner=document.querySelectorAll('#dinner input[type="number"]');
+  const snacks=document.querySelectorAll('#snacks input[type="number"]');
+  const exercise=document.querySelectorAll('#exercise input[type="number"]');
+  const lunch=document.querySelectorAll('#lunch input[type="number"]');
+
+  const breakfastCalories=addCalories(breakfast);
+  const lunchCalories=addCalories(lunch);
+  const dinnerCalories=addCalories(dinner);
+  const snacksCalories=addCalories(snacks);
+  const exerciseCalories=addCalories(exercise);
+  const burgetedCalories=addCalories([caloriesBudgetInput])
+  const consumedCalories=breakfastCalories+lunchCalories+dinnerCalories+snacksCalories;
+  const burnedCalories=exerciseCalories;
+  const calories=burgetedCalories-consumedCalories+burnedCalories;
+  const DeficitOrSurplus=burgetedCalories-consumedCalories+burnedCalories<0?'Surplus':'Deficit';
+  console.log('consumedCalories',consumedCalories);
+  console.log('burnedCalories',burnedCalories);
+  console.log('burgetedCalories',burgetedCalories);
+    
+  caloriesBudgetText.innerHTML=`Burgeted Calories ${burgetedCalories}`;
+  consumedCaloriesText.innerHTML=`Consumed Calories ${consumedCalories}`;
+  burnedcaloriesText.innerHTML=`Burned Calories ${burnedCalories}`;
+  DeficitOrSurplusText.innerHTML=`${DeficitOrSurplus} of ${Math.abs(calories)}`;
+  output.classList.remove('hide')
+ }
+submitButton.onclick=(e)=>{
+e.preventDefault();
+calculateCalories();
+}
+ 
+const reset=()=>{
+    for( const node of caloriesInput){
+        node.innerHTML='';
+    }
+    caloriesBudgetInput.innerHTML='';
+    output.classList.add('hide');
+}
+
+resetButton.onclick=reset;
+
+
+ 
